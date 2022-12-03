e91e44741c4be1893cc0fa008540f7f66e41662f
Chandan Ojha <chandanojha776@gmail.com>
2022-12-03 20:42:30 +0600

    employees api developed

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -37,6 +37,28 @@ class EmployeeController extends BaseController
 
     /*
      * API: 2
+     * Purpose: Get Single Employee Information
+     * Route: api/pp/get-single-employee-info
+     * Method: Get
+     * Parameter: emp_id
+    */
+    public function get_single_employee_info($emp_id){
+        $employee_info = Employee::where('id',$emp_id)->first();
+
+        //~ Check Availability of data
+        if(!empty($employee_info)){
+            return $this->sendResponse(200,'Success.',
+                [
+                    'employee_info' => $employee_info
+                ]
+            );
+        }
+        //~ Empty Data Response
+        return $this->sendError('204','Data not found!');
+    }
+
+    /*
+     * API: 3
      * Purpose: Create Employee
      * Route: api/pp/create-employee
      * Method: Post
@@ -53,7 +75,13 @@ class EmployeeController extends BaseController
         {
             return response()->json([
                 'status_code' =>200,
-                'message' =>"Employee created successfully!"
+                'message' =>"Employee created successfully!",
+                'id' => $employee->id,
+                'name' => $employee->name,
+                'position' => $employee->position,
+                'age' => $employee->age,
+                'start_date' => $employee->start_date,
+                'salary' => $employee->salary
             ]);
         }
         else
@@ -63,5 +91,69 @@ class EmployeeController extends BaseController
                 'message' =>"Employee not created!"
             ]);
         }
+    }
+
+    /*
+     * API: 4
+     * Purpose: Update Employee
+     * Route: api/pp/update-employee
+     * Method: Put
+     * Parameter: emp_id
+    */
+    public function update_employee(Request $request, $emp_id){
+        $update_employee = Employee::where('id',$emp_id)->first();
+        $update_employee->name = $request->name;
+        $update_employee->position = $request->position;
+        $update_employee->age = $request->age;
+        $update_employee->start_date = $request->start_date;
+        $update_employee->salary = $request->salary;
+
+        if($update_employee->save())
+        {
+            return response()->json([
+                'status_code' =>200,
+                'message' =>"Employee updated successfully!",
+                'id' => $update_employee->id,
+                'name' => $update_employee->name,
+                'position' => $update_employee->position,
+                'age' => $update_employee->age,
+                'start_date' => $update_employee->start_date,
+                'salary' => $update_employee->salary
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status_code' =>400,
+                'message' =>"Employee not updated !"
+            ]);
+        }
+
+    }
+
+    /*
+     * API: 5
+     * Purpose: Delete Employee
+     * Route: api/pp/delete-employee
+     * Method: delete
+     * Parameter: emp_id
+    */
+    public function delete_employee($emp_id){
+        $employee = Employee::where('id',$emp_id)->first();
+
+        if($employee->delete())
+        {
+            return response()->json([
+                'status_code' =>200,
+                'message' =>"Employee deleted successfully!"
+            ]);
+        }
+        else {
+            return response()->json([
+                'status_code' =>400,
+                'message' =>"Employee not deleted!"
+            ]);
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('get-employee-list',[EmployeeController::class,'get_employee_list']);
+Route::get('get-single-employee-info/{emp_id}',[EmployeeController::class,'get_single_employee_info']);
 Route::post('create-employee',[EmployeeController::class,'create_employee']);
+Route::put('update-employee/{emp_id}',[EmployeeController::class,'update_employee']);
+Route::delete('delete-employee/{emp_id}',[EmployeeController::class,'delete_employee']);
