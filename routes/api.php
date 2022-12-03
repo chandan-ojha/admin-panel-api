a3fb6e63c5eec6bbb742b6245adb0685af35cd14
Chandan Ojha <chandanojha776@gmail.com>
2022-12-04 00:22:55 +0600

    user credentials api developed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('get-employee-list',[EmployeeController::class,'get_employee_list']);
-Route::get('get-single-employee-info/{emp_id}',[EmployeeController::class,'get_single_employee_info']);
-Route::post('create-employee',[EmployeeController::class,'create_employee']);
-Route::put('update-employee/{emp_id}',[EmployeeController::class,'update_employee']);
-Route::delete('delete-employee/{emp_id}',[EmployeeController::class,'delete_employee']);
+/*
+ *  User Credentials
+ */
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::get('get-employee-list',[EmployeeController::class,'get_employee_list']);
+    Route::get('get-single-employee-info/{emp_id}',[EmployeeController::class,'get_single_employee_info']);
+    Route::post('create-employee',[EmployeeController::class,'create_employee']);
+    Route::put('update-employee/{emp_id}',[EmployeeController::class,'update_employee']);
+    Route::delete('delete-employee/{emp_id}',[EmployeeController::class,'delete_employee']);
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+
+
